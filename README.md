3331ed4002fd83ef1c0671ad2663cf333cf1874e
sung8 <heechuw@gmail.com>
2024-06-20 15:50:02 -0400

    original

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# custom_newtab
+# Custom New Tab Page
+
+Catppuccin
+
+https://github.com/pivoshenko/catppuccin-startpage/assets/40499728/d96c8bd6-168e-408f-b4f0-0e339569c696
+
+> [!NOTE]
+> A live demo can be found [here](https://pivoshenko.github.io/catppuccin-startpage)
+
+## Overview
+
+Clone of [pivoshenko](https://github.com/pivoshenko/catppuccin-startpage)'s Catppuccin-themed Browser New Tab/Startup Page.
+
+Full credits to [pivoshenko](https://github.com/pivoshenko/).
+
+## Usage
+
+1. Create your own repository for the startpage and clone it
+2. Clone this repository with:
+
+```shell
+git clone https://github.com/pivoshenko/catppuccin-startpage.git pivoshenko-catppuccin-startpage
+```
+
+3. Copy the assets and configs from `pivoshenko-catppuccin-startpage` to your repository:
+
+```shell
+cp pivoshenko-catppuccin-startpage/* <YOUR REPOSITORY>
+```
+
+4. Remove `.github` directory as it contains only PR templates, issue labels etc that are linked to this repository
+5. Update [`userconfig.js`]:
+   - Set your location for the weather widget
+   - Update the number of pages and their banners
+   - Update bookmarks and quick links for the one you are using the most :3
+
+> [!TIP]
+> You can find icons for your bookmarks using [`tabler-icons`]
+>
+> If you want to reduce the loading time of the icons, you could install the icon [font] locally and activate the option `"localIcons": true` in the config to disable the remote styles
+
+#### As Homepage
+
+- Click the menu button and select `Options/Preferences`
+- Click the home panel
+- Click the menu next to the homepage and new windows and choose to show custom URLs and add your GitHub Pages link
+
+#### As New Tab
+
+You can use different Add-ons/Extensions for it
+
+- If you use Firefox: [Custom New Tab Page](https://addons.mozilla.org/en-US/firefox/addon/custom-new-tab-page/?src=search) and make sure you enable "Force links to open in the top frame (experimental)" in the extension's preferences page
+- If you use Chromium (Brave, Chrome): [Custom New Tab URL](https://chrome.google.com/webstore/detail/custom-new-tab-url/mmjbdbjnoablegbkcklggeknkfcjkjia)
+
+[`dawn`]: https://github.com/b-coimbra/dawn
+[Catppuccin]: https://github.com/catppuccin/catppuccin
+[`tartarus-startpage`]: https://github.com/AllJavi/tartarus-startpage
+[`dotfiles`]: https://github.com/pivoshenko/dotfiles
+[`userconfig.js`]: userconfig.js
+[`tabler-icons`]: https://tabler.io/icons
+[font]: src/fonts
---
+++ userconfig.js
@@ -0,0 +1,275 @@
+let saved_config = JSON.parse(localStorage.getItem("CONFIG"));
+
+const default_config = {
+  overrideStorage: true,
+  temperature: {
+    location: "London",
+    scale: "C",
+  },
+  clock: {
+    format: "h:i p",
+    iconColor: "#f38ba8",
+  },
+  disabled: [],
+  fastlink: "https://app.raindrop.io",
+  openLastVisitedTab: true,
+  tabs: [
+    {
+      name: "myself",
+      background_url: "src/img/banners/cbg-9.gif",
+      categories: [
+        {
+          name: "bookmarks",
+          links: [
+            {
+              name: "raindrop",
+              url: "https://app.raindrop.io",
+              icon: "droplet-bolt",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "musicForProgramming();",
+              url: "https://musicforprogramming.net/sixty",
+              icon: "binary-tree",
+              icon_color: "#fab387",
+            },
+          ],
+        },
+        {
+          name: "workspace",
+          links: [
+            {
+              name: "gmail",
+              url: "https://mail.google.com",
+              icon: "brand-gmail",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "calendar",
+              url: "https://calendar.google.com",
+              icon: "calendar-filled",
+              icon_color: "#fab387",
+            },
+            {
+              name: "sheets",
+              url: "https://docs.google.com/spreadsheets",
+              icon: "table",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "drive",
+              url: "https://drive.google.com/drive/home",
+              icon: "brand-google-drive",
+              icon_color: "#89b4fa",
+            },
+          ],
+        },
+        {
+          name: "media",
+          links: [
+            {
+              name: "уп",
+              url: "https://www.pravda.com.ua",
+              icon: "news",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "mil.in.ua",
+              url: "https://mil.in.ua",
+              icon: "badge-filled",
+              icon_color: "#fab387",
+            },
+            {
+              name: "куток",
+              url: "https://kutok.io",
+              icon: "border-radius",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "ґрунт",
+              url: "https://grnt.media",
+              icon: "eye-bolt",
+              icon_color: "#89b4fa",
+            },
+            {
+              name: "village",
+              url: "https://www.village.com.ua",
+              icon: "home-2",
+              icon_color: "#cba6f7",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "dev",
+      background_url: "src/img/banners/cbg-8.gif",
+      categories: [
+        {
+          name: "resources",
+          links: [
+            {
+              name: "github",
+              url: "https://github.com",
+              icon: "brand-github",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "neptune",
+              url: "https://ui.neptune.ai",
+              icon: "circle-triangle",
+              icon_color: "#fab387",
+            },
+            {
+              name: "wakatime",
+              url: "https://wakatime.com/dashboard",
+              icon: "24-hours",
+              icon_color: "#f38ba8",
+            },
+          ],
+        },
+        {
+          name: "challenges",
+          links: [
+            {
+              name: "kaggle",
+              url: "https://www.kaggle.com/volodymyrpivoshenko",
+              icon: "brain",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "leetcode",
+              url: "https://leetcode.com",
+              icon: "code-plus",
+              icon_color: "#fab387",
+            },
+            {
+              name: "stackoverflow",
+              url: "https://stackoverflow.com",
+              icon: "brand-stackoverflow",
+              icon_color: "#f38ba8",
+            },
+          ],
+        },
+        {
+          name: "blogs",
+          links: [
+            {
+              name: "dou",
+              url: "https://dou.ua",
+              icon: "brand-prisma",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "hackernews",
+              url: "https://news.ycombinator.com",
+              icon: "brand-redhat",
+              icon_color: "#fab387",
+            },
+            {
+              name: "uber research",
+              url: "https://eng.uber.com/category/articles",
+              icon: "brand-uber",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "google research",
+              url: "https://blog.research.google",
+              icon: "hexagon-letter-g",
+              icon_color: "#89b4fa",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "chi ll",
+      background_url: "src/img/banners/cbg-10.gif",
+      categories: [
+        {
+          name: "social medias",
+          links: [
+            {
+              name: "telegram",
+              url: "https://web.telegram.org",
+              icon: "brand-telegram",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "facebook",
+              url: "https://www.facebook.com",
+              icon: "brand-facebook",
+              icon_color: "#fab387",
+            },
+            {
+              name: "reddit",
+              url: "https://www.reddit.com/r/unixporn",
+              icon: "brand-reddit",
+              icon_color: "#f38ba8",
+            },
+          ],
+        },
+        {
+          name: "games",
+          links: [
+            {
+              name: "GG",
+              url: "https://ggapp.io/pivoshenko",
+              icon: "device-gamepad",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "steam",
+              url: "https://store.steampowered.com",
+              icon: "brand-steam",
+              icon_color: "#fab387",
+            },
+            {
+              name: "epicgames",
+              url: "https://store.epicgames.com",
+              icon: "brand-fortnite",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "nintendo",
+              url: "https://store.nintendo.co.uk",
+              icon: "device-nintendo",
+              icon_color: "#89b4fa",
+            },
+          ],
+        },
+        {
+          name: "video",
+          links: [
+            {
+              name: "anilist",
+              url: "https://anilist.co/home",
+              icon: "brand-funimation",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "youtube",
+              url: "https://www.youtube.com",
+              icon: "brand-youtube",
+              icon_color: "#fab387",
+            },
+            {
+              name: "patreon",
+              url: "https://www.patreon.com",
+              icon: "brand-patreon",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "kyivstar",
+              url: "https://tv.kyivstar.ua",
+              icon: "star-filled",
+              icon_color: "#89b4fa",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const CONFIG = new Config(default_config);
